f74516bfce1dc044dcc51e265ca5d738204b5eba
Finalwinner <2473113916@qq.com>
2021-04-14 20:56:29 +0800

    finish list.c

--- file_system/list.c
+++ file_system/list.c
@@ -1,4 +1,5 @@
-/*ls -l <dir_name>
+/*
+*ls -l <dir_path>
  * mode links Uid Gid Size mtime filename
  * */
 #include<sys/types.h>
@@ -8,6 +9,10 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<dirent.h>
+#include<pwd.h>
+#include<grp.h>
+#include<time.h>
+#include<string.h>
 
 void printf_type(unsigned char type)
 {
@@ -68,45 +73,70 @@ void print_mode(unsigned int mode)
 	}
 }
 
-int main(void)
+void list_file(char *rootpath)
 {
 	DIR *dir;
 	struct dirent *dirp;
 	char path[1024];
-	char *rootpath = ".";
-	struct stat *file_stat;
-	
+	struct stat file_stat;
+	struct passwd *pwd;
+	struct group *gr;
+	struct tm lt;//local time
+	char mtime[80];
+	const char *c_path = ".";//current path
+	const char *f_path = "..";
+
 	if((dir = opendir(rootpath)) == NULL){
 		perror("opendir");
+		exit(-1);
 	}
 
 	//read the dirent one by one
 	while((dirp = readdir(dir)) != NULL){
-		//if((dirp->d_name == ".") || (dirp->d_name == ".."))
-		//	continue;
+		//ignore . and ..
+		if((strncmp(dirp->d_name,c_path,sizeof(c_path)) == 0) || (strncmp(dirp->d_name,f_path,sizeof(f_path)) == 0))
+			continue;
 		//print the type of the file
 		printf_type(dirp->d_type);
 
 		sprintf(path,"%s/%s",rootpath,dirp->d_name);
-		if(stat(path,file_stat) == -1){
+
+		if(stat(path,&file_stat) == -1){
 			perror("stat");
 			exit(-1);
 		}
 		//print the mode
-		print_mode(file_stat->st_mode & 0700/(8*8));
-		print_mode(file_stat->st_mode & 0070/8);
-		print_mode(file_stat->st_mode & 0007);
+		print_mode(file_stat.st_mode & 0700/(8*8));
+		print_mode(file_stat.st_mode & 0070/8);
+		print_mode(file_stat.st_mode & 0007);
 
 		//print links,Gid,Uid , can find the user name by file passwd 
-		printf(" %lu  %u   %u   ",file_stat->st_nlink,file_stat->st_gid,file_stat->st_uid);
+		pwd = getpwuid(file_stat.st_uid);
+		gr = getgrgid(file_stat.st_gid);
+		printf(" %3lu %s %s ",file_stat.st_nlink,pwd->pw_name,gr->gr_name);
 
+		//print size
+		printf("%8ld ",file_stat.st_size);
 		//print the mtime
-		printf("%lu   ",file_stat->st_ctime);
+		localtime_r(&(file_stat.st_mtime),&lt);
+		strftime(mtime,sizeof(mtime),"%c",&lt);
+		printf("%s ",mtime);
 
 		//print the file_name
 		printf("%s \n",dirp->d_name);	
 	}
-
 	closedir(dir);
+}
+
+int main(int argc,char *argv[])
+{
+	if(argc < 2){
+		list_file(".");
+	}else{
+		for(int i = 1;i < argc;i++){
+			printf("%s :\n",argv[i]);
+			list_file(argv[i]);
+		}
+	}
 	return 0;
 }
